3761b1399bb3fbed38a1b790e8c6f2343bffd770
BIOhazzardMTV <fedorov200375@mail.ru>
2022-10-03 20:48:55 +0400

    Изменил ссылку поиска яндекс картинок, теперь ищутся только изображения (без gif); Добавил обработку ещё нескольких исключений; Изменил функцию full_save: теперь в качестве параметра можно задавать число скачиваемых картинок

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import socket
 import requests
+import urllib3.exceptions
 from bs4 import BeautifulSoup
 import os
 import time
 
 
 def get_url(num, name):
-    url = f"https://yandex.ru/images/search?p={num}&text={name}"
+    url = f"https://yandex.ru/images/search?p={num}&text={name}&itype=jpg"
     html_page = requests.get(url)
     return html_page
 
@@ -16,7 +18,7 @@ def save_img(link, class_name, img_name):
             r = requests.get(link, timeout=1)
             if r:
                 os.chdir(f"{class_name}")
-                filename = f'{img_name:03}.jpg'
+                filename = f'{img_name:04}.jpg'
                 # print(filename)
                 open(filename, "wb",).write(r.content)
                 os.chdir("..")
@@ -24,31 +26,38 @@ def save_img(link, class_name, img_name):
                 return True
         return False
     except requests.exceptions.Timeout:
-        print("Time out")
+        return False
+    except socket.timeout:
+        return False
+    except urllib3.exceptions.ReadTimeoutError:
+        return False
+    except requests.exceptions.ConnectionError:
+        return False
 
 
-def full_save(class_name):
+def full_save(class_name, num_of_pictures):
     page_num = 0
     soup = BeautifulSoup(get_url(page_num, f'{class_name}').text, "lxml")
     n = 29
     i = 0
     j = 0
-    while j != 100:
-        time.sleep(0.05)
+    while j != num_of_pictures:
+        time.sleep(0.02)
         data = str(soup.find("div", class_=f"serp-item_pos_{i}"))
-        data = data.split('origin":')
-        data = data[1].split('url":"')
-        data = data[1].split('"},')
-        print(j, "", data[0])
-        if save_img(data[0], f"{class_name}", j):
-            print("Success")
-            j += 1
-        else:
-            print("Error")
-        if i == n:
-            page_num += 1
-            soup = BeautifulSoup(get_url(page_num, f'{class_name}').text, "lxml")
-            n += 30
+        if data.find('"origin":{"') != -1:
+            data = data.split('"origin":{"')
+            data = data[1].split('"url":"')
+            data = data[1].split('"},"')
+            print("i = ", i, ", " + "j = ", j, ", " + "page_num = ", page_num, " ", data[0])
+            if save_img(data[0], class_name, j):
+                print("Success")
+                j += 1
+            else:
+                print("Error")
+            if i == n:
+                page_num += 1
+                soup = BeautifulSoup(get_url(page_num, f'{class_name}').text, "lxml")
+                n += 30
         i += 1
 
 
@@ -63,6 +72,17 @@ else:
         os.mkdir("cat")
     if not os.path.isdir("dog"):
         os.mkdir("dog")
-full_save("cat")
-full_save("dog")
+full_save("cat", 1200)
+full_save("dog", 1200)
 # while (len(os.listdir(f"dataset/{class_name}")) != 1000):
+# soup = BeautifulSoup(get_url(4, 'cat').content, "lxml")
+# data = str(soup.find("div", class_="serp-item_pos_121"))
+# data = data.split('"origin":{"')
+# print(data)
+# data = data[1].split('"url":"')
+# data = data[1].split('"},"')
+# print("i = ", 121, ", " + "j = ", 9999, ", " + "page_num = ", 4, " ", data[0])
+# if save_img(data[0], "cat", 9999):
+#     print("Success")
+# else:
+#     print("Error")
