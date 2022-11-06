184ef2fabe1097b1800e65003db18048c8bbdf31
BIOhazzardMTV <fedorov200375@mail.ru>
2022-11-06 19:37:22 +0400

    Удалил функцию get_url, переместил её в тело функции full_save, так же изменил метод получения html-кода - теперь создаётся объект сессии, у которого я затем запрашиваю информацию, что должно привести к повышению производительности. Добавил отображение прогресса загрузки - после каждой попытки загрузки изображения пишется общий процент скачанных файлов заданного класса. Добавил сохранение информации о количестве скачанных файлов и последнем рассмотренном изображении в текстовый файл, для каждого класса создаётся отдельный файл.

--- main.py
+++ main.py
@@ -2,63 +2,68 @@ import socket
 import requests
 import urllib3.exceptions
 from bs4 import BeautifulSoup
+import ssl
 import os
 import time
 
 
-def get_url(num, name):
-    url = f"https://yandex.ru/images/search?p={num}&text={name}&itype=jpg"
-    html_page = requests.get(url)
-    return html_page
-
-
-def save_img(link, class_name, img_name):
+def save_img(link, img_name):
     try:
         if link.find(".jpg") != -1:
-            r = requests.get(link, timeout=1)
+            r = requests.get(link, timeout=2)
             if r:
-                os.chdir(f"{class_name}")
                 filename = f'{img_name:04}.jpg'
-                # print(filename)
-                open(filename, "wb",).write(r.content)
-                os.chdir("..")
-                # print(len(os.listdir(f"{class_name}")))
+                open(filename, "wb").write(r.content)
                 return True
+    except requests.exceptions.Timeout or \
+            socket.timeout or \
+            urllib3.exceptions.ReadTimeoutError or \
+            requests.exceptions.ConnectionError or \
+            requests.exceptions.SSLError or \
+            urllib3.exceptions.MaxRetryError or \
+            ssl.SSLCertVerificationError:
         return False
-    except requests.exceptions.Timeout:
-        return False
-    except socket.timeout:
-        return False
-    except urllib3.exceptions.ReadTimeoutError:
-        return False
-    except requests.exceptions.ConnectionError:
-        return False
+    return False
 
 
 def full_save(class_name, num_of_pictures):
+    print(f"Начало загрузки изображений класса {class_name}")
+    os.chdir(f"{class_name}")
+    if not os.path.isfile(f"info-{class_name}.txt"):
+        i = 0
+        j = 0
+    else:
+        i = int(open(f"info-{class_name}.txt", "r").readline())
+        j = int(open(f"info-{class_name}.txt", "r").readline(2))
+    num_of_uploaded = 0
     page_num = 0
-    soup = BeautifulSoup(get_url(page_num, f'{class_name}').text, "lxml")
     n = 29
-    i = 0
-    j = 0
-    while j != num_of_pictures:
-        time.sleep(0.02)
+    session = requests.session()
+    response = session.get(f"https://yandex.ru/images/search?p={page_num}&text={class_name}", timeout=2)
+    soup = BeautifulSoup(response.text, "lxml")
+    while num_of_uploaded != num_of_pictures:
+        time.sleep(0.05)
         data = str(soup.find("div", class_=f"serp-item_pos_{i}"))
         if data.find('"origin":{"') != -1:
             data = data.split('"origin":{"')
             data = data[1].split('"url":"')
-            data = data[1].split('"},"')
-            print("i = ", i, ", " + "j = ", j, ", " + "page_num = ", page_num, " ", data[0])
-            if save_img(data[0], class_name, j):
-                print("Success")
+            data = data[1].split('"}')
+            # print(f"i = {i}; " + f"j = {j}; " + f"page_num = {page_num}  " + f"link : {data[0]}")
+            if save_img(data[0], j):
+                print(f'{round((100 * num_of_uploaded / num_of_pictures), 2)}%')
                 j += 1
+                num_of_uploaded += 1
             else:
-                print("Error")
+                print(f'{round((100 * num_of_uploaded / num_of_pictures), 2)}%')
             if i == n:
                 page_num += 1
-                soup = BeautifulSoup(get_url(page_num, f'{class_name}').text, "lxml")
+                soup = BeautifulSoup(response.text, "lxml")
                 n += 30
         i += 1
+        open(f"info-{class_name}.txt", "w+").write(f"{i}\n{j}")
+    os.chdir("..")
+    print(f'Изображения класса "{class_name}" в количестве {num_of_pictures} шт. успешно завершена!')
+    print("")
 
 
 if not os.path.isdir("dataset"):
@@ -72,17 +77,6 @@ else:
         os.mkdir("cat")
     if not os.path.isdir("dog"):
         os.mkdir("dog")
-full_save("cat", 1200)
-full_save("dog", 1200)
-# while (len(os.listdir(f"dataset/{class_name}")) != 1000):
-# soup = BeautifulSoup(get_url(4, 'cat').content, "lxml")
-# data = str(soup.find("div", class_="serp-item_pos_121"))
-# data = data.split('"origin":{"')
-# print(data)
-# data = data[1].split('"url":"')
-# data = data[1].split('"},"')
-# print("i = ", 121, ", " + "j = ", 9999, ", " + "page_num = ", 4, " ", data[0])
-# if save_img(data[0], "cat", 9999):
-#     print("Success")
-# else:
-#     print("Error")
+full_save("cat", 11)
+time.sleep(0.01)
+full_save("dog", 11)
