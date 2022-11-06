6bfe3d5d36ef352497970330c1b1708fc3156715
BIOhazzardMTV <fedorov200375@mail.ru>
2022-11-06 19:38:02 +0400

    Исправление ошибок

--- main.py
+++ main.py
@@ -1,69 +1,75 @@
-import socket
+# import socket
 import requests
 import urllib3.exceptions
 from bs4 import BeautifulSoup
-import ssl
 import os
 import time
+urllib3.disable_warnings()
 
 
-def save_img(link, img_name):
+def save_img(link, img_name, session):
     try:
         if link.find(".jpg") != -1:
-            r = requests.get(link, timeout=2)
+            r = session.get(link, timeout=2, verify=False, allow_redirects=True)
             if r:
                 filename = f'{img_name:04}.jpg'
                 open(filename, "wb").write(r.content)
                 return True
-    except requests.exceptions.Timeout or \
-            socket.timeout or \
-            urllib3.exceptions.ReadTimeoutError or \
-            requests.exceptions.ConnectionError or \
-            requests.exceptions.SSLError or \
-            urllib3.exceptions.MaxRetryError or \
-            ssl.SSLCertVerificationError:
-        return False
+    except Exception:
+        print("Timeout")
     return False
 
 
-def full_save(class_name, num_of_pictures):
-    print(f"Начало загрузки изображений класса {class_name}")
-    os.chdir(f"{class_name}")
+def full_save(class_name, num_of_required_pictures):
+    print(f'Начало загрузки изображений класса "{class_name}"')
+    os.chdir(f'{class_name}')
     if not os.path.isfile(f"info-{class_name}.txt"):
         i = 0
         j = 0
+        page_num = 0
     else:
-        i = int(open(f"info-{class_name}.txt", "r").readline())
-        j = int(open(f"info-{class_name}.txt", "r").readline(2))
+        file = open(f"info-{class_name}.txt", "r")
+        i = int(file.readline())
+        j = int(file.readline())
+        page_num = int(file.readline())
+        file.close()
     num_of_uploaded = 0
-    page_num = 0
-    n = 29
+    n = 29 + 30 * page_num
     session = requests.session()
     response = session.get(f"https://yandex.ru/images/search?p={page_num}&text={class_name}", timeout=2)
     soup = BeautifulSoup(response.text, "lxml")
-    while num_of_uploaded != num_of_pictures:
-        time.sleep(0.05)
+    num_of_errors_in_a_row = 0
+    while num_of_uploaded < num_of_required_pictures:
+        time.sleep(0.2)
         data = str(soup.find("div", class_=f"serp-item_pos_{i}"))
         if data.find('"origin":{"') != -1:
             data = data.split('"origin":{"')
             data = data[1].split('"url":"')
             data = data[1].split('"}')
             # print(f"i = {i}; " + f"j = {j}; " + f"page_num = {page_num}  " + f"link : {data[0]}")
-            if save_img(data[0], j):
-                print(f'{round((100 * num_of_uploaded / num_of_pictures), 2)}%')
+            if save_img(data[0], j, session):
                 j += 1
                 num_of_uploaded += 1
-            else:
-                print(f'{round((100 * num_of_uploaded / num_of_pictures), 2)}%')
-            if i == n:
-                page_num += 1
-                soup = BeautifulSoup(response.text, "lxml")
-                n += 30
+                num_of_errors_in_a_row = 0
+        else:
+            num_of_errors_in_a_row += 1
+        print(f'{round((100 * num_of_uploaded / num_of_required_pictures), 2)}%')
+        if i >= n:
+            page_num += 1
+            response = session.get(f"https://yandex.ru/images/search?p={page_num}&text={class_name}", timeout=2)
+            soup = BeautifulSoup(response.text, "lxml")
+            n += 30
         i += 1
-        open(f"info-{class_name}.txt", "w+").write(f"{i}\n{j}")
-    os.chdir("..")
-    print(f'Изображения класса "{class_name}" в количестве {num_of_pictures} шт. успешно завершена!')
-    print("")
+        open(f"info-{class_name}.txt", "w+").write(f"{i}\n{j}\n{page_num}")
+        if num_of_errors_in_a_row == 10:
+            break
+    if num_of_uploaded != num_of_required_pictures:
+        print(f'Удалось скачать только {num_of_uploaded} шт. изображений класса "{class_name}"'
+              f'(требовалось {num_of_required_pictures} шт.) ')
+        print("")
+    else:
+        print(f'Изображения класса "{class_name}" в количестве {num_of_uploaded} шт. успешно завершена!')
+        print("")
 
 
 if not os.path.isdir("dataset"):
@@ -77,6 +83,8 @@ else:
         os.mkdir("cat")
     if not os.path.isdir("dog"):
         os.mkdir("dog")
-full_save("cat", 11)
-time.sleep(0.01)
-full_save("dog", 11)
+try:
+    full_save('dog', 162)
+except Exception:
+    print("Error")
+
